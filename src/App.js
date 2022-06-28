bb62dddc2bd906f73c89f65f4e2704147a25b4a1
abdubosit-js <abdubositjs@gmail.com>
2022-06-28 18:24:03 +0500

    react-test

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = "abdubosit"
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          salom {name}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
