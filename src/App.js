1d5134b3fa7f505fe5d14a7b10c82ba058f76480
abdubosit-js <abdubositjs@gmail.com>
2022-06-28 19:20:19 +0500

    test

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          salom {name}
-        </p>
+        <p>salom {name}</p>
       </header>
     </div>
   );
